180e9108d74be0d20cedd25092e8d7796da097cc
Alaminos <aalaminosz@gmail.com>
2018-09-03 12:34:38 +0200

    update

--- src/components/GetInfo.js
+++ src/components/GetInfo.js
@@ -1,88 +1,91 @@
-import React from 'react';
-import axios from 'axios';
-import githubUsernameRegex from 'github-username-regex';
+import React from "react";
+import axios from "axios";
+import githubUsernameRegex from "github-username-regex";
 
-import Header from './Header';
-import Content from './Content';
-import Footer from './Footer';
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
 
 const id = "f5ce4435afdfe23711c6";
 const sec = "1daa19d525b9e92f034ebe504075e5b1600eea46";
 const params = "?client_id=" + id + "&client_secret=" + sec;
 
 class GetInfo extends React.Component {
-	constructor (props) {
-		super(props);
-		this.state = { 
-			input: 'getify',
-			user: null,
-			repos: null,
-			isInvalid: false,
-			isFound: true,
-		}
-		this.getInfo = this.getInfo.bind(this);
-		this.inputHandler = this.inputHandler.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "getify",
+      user: null,
+      repos: null,
+      isInvalid: false,
+      isFound: true
+    };
+    this.getInfo = this.getInfo.bind(this);
+    this.inputHandler = this.inputHandler.bind(this);
+  }
 
-	componentDidMount() {
-		this.getInfo();
-	}
+  componentDidMount() {
+    this.getInfo();
+  }
 
-	getInfo() {
-		const username = this.state.input;
-		// Check if the username is a valid one
-		if(!githubUsernameRegex.test(username)) {
-			// if not, set state to reflect so
-			// and exit the function without calling the API
-			this.setState({isInvalid: true})
-			return;
-		} else { 
-			this.setState({isInvalid: false})
-		}
-		// Get user data
-		const userUrl = 'https://api.github.com/users/' + username + params;
-		const reposUrl = 'https://api.github.com/users/' + username + '/repos' + params + '&per_page=100';
-		
-		axios.all([ 
-			axios.get(userUrl), axios.get(reposUrl) 
-			])
-			.then(axios.spread((userResp, reposResp) => {
-					this.setState({
-						user: userResp.data,
-						repos: reposResp.data,
-						isFound:true
-					});
-				}))
-				.catch((error) => {
-    				console.log('FAIL', error);
-    				if(error.response.status === 404){
-						this.setState({isFound: false});
-					}
-  				});
-	}
+  getInfo() {
+    const username = this.state.input;
+    // Check if the username is a valid one
+    if (!githubUsernameRegex.test(username)) {
+      // if not, set state to reflect so
+      // and exit the function without calling the API
+      this.setState({ isInvalid: true });
+      return;
+    } else {
+      this.setState({ isInvalid: false });
+    }
+    // Get user data
+    const userUrl = "https://api.github.com/users/" + username + params;
+    const reposUrl =
+      "https://api.github.com/users/" +
+      username +
+      "/repos" +
+      params +
+      "&per_page=100";
 
-	inputHandler(event) {
-		const input = event.target.value;
-		this.setState({input});
-	}
+    axios
+      .all([axios.get(userUrl), axios.get(reposUrl)])
+      .then(
+        axios.spread((userResp, reposResp) => {
+          this.setState({
+            user: userResp.data,
+            repos: reposResp.data,
+            isFound: true
+          });
+        })
+      )
+      .catch(error => {
+        console.log("FAIL", error);
+        if (error.response.status === 404) {
+          this.setState({ isFound: false });
+        }
+      });
+  }
 
-	render() {
-		return (
-			<div className='wrapper'>
-				<Header 
-					change={this.inputHandler}
-					click={this.getInfo}
-				/>
-				<Content 
-					isInvalid={this.state.isInvalid}
-					isFound={this.state.isFound}
-					user={this.state.user}
-					repos={this.state.repos} 
-				/>
-				<Footer />
-			</div>
-		);
-	}
+  inputHandler(event) {
+    const input = event.target.value;
+    this.setState({ input });
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <Header change={this.inputHandler} click={this.getInfo} />
+        <Content
+          isInvalid={this.state.isInvalid}
+          isFound={this.state.isFound}
+          user={this.state.user}
+          repos={this.state.repos}
+        />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default GetInfo;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,41 @@
-import React from 'react';
-import logo from '../logo.png';
+import React from "react";
+import logo from "../logo.png";
 
 function keyDownHandler(event) {
-	if (event.keyCode === 13) {
-		document.getElementById('searchButton').click();
-	}
+  if (event.keyCode === 13) {
+    document.getElementById("searchButton").click();
+  }
 }
 
-const Header = (props) => {
-	return(
-		<div className='header'>
-			<img className='logo' src={logo} alt='Github Profile Display Logo' />
-			<h1 className='title' >Github Profiles</h1>
-			<div className='userSearch'>
-				<input className='searchInput' id='searchInputID'
-					type="text" name="fname" placeholder="Search for a user"
-				
-					onChange={props.change}
-					onKeyDown={keyDownHandler}
-				/>
-				<button className='searchBtn' id='searchButton' onClick={props.click}>Get info</button>
-			</div>
-			<div className="a2a_kit a2a_kit_size_32 a2a_default_style icons-header">
-				<a className="a2a_dd" href="https://www.addtoany.com/share">Social Share</a>
-				<a className="a2a_button_copy_link">Copy Link</a>
-			</div>
-		</div>
-	);
-}
+const Header = props => {
+  return (
+    <div className="header">
+      <a className="logo-box" href="/">
+        <img className="logo" src={logo} alt="Github Profile Display Logo" />
+      </a>
+      <h1 className="title">Github Profiles</h1>
+      <div className="userSearch">
+        <input
+          className="searchInput"
+          id="searchInputID"
+          type="text"
+          name="fname"
+          placeholder="Search for a user"
+          onChange={props.change}
+          onKeyDown={keyDownHandler}
+        />
+        <button className="searchBtn" id="searchButton" onClick={props.click}>
+          Get info
+        </button>
+      </div>
+      <div className="a2a_kit a2a_kit_size_32 a2a_default_style icons-header">
+        <a className="a2a_dd" href="https://www.addtoany.com/share">
+          Social Share
+        </a>
+        <a className="a2a_button_copy_link">Copy Link</a>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
